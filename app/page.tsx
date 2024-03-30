812191f44f2df98bd4780fcfb0b83c5a94b0c994
bxokhxnhle <khanh.le@uwaterloo.ca>
2024-03-30 00:46:58 -0400

    changed spotify widget

--- app/page.tsx
+++ app/page.tsx
@@ -44,9 +44,9 @@ export default function Home() {
               <div className="spotify-container flex items-center p-5">
                 <BsSpotify className="opacity-90 spotify-icon shrink-0" size="40px" alt="Spotify" />
                 <div className="ml-6 md:ml-5">
-                  <SpotifyCurrentlyPlaying />
+                  {/* <SpotifyCurrentlyPlaying /> */}
                   {/* <SpotifyRecentlyPlayed /> */}
-                  {/* <SpotifyOnRepeat /> */}
+                  <SpotifyOnRepeat />
                 </div>
               </div>
             </div>
