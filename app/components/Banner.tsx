6736d61e89bd5fafe36c9ddbbc972fe0728b287c
bxokhxnhle <khanh.le@uwaterloo.ca>
2024-04-07 01:53:41 -0400

    fixed alt bug

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -37,10 +37,10 @@ export default function Banner() {
           <div className="font-medium text-4xl md:text-5xl h-32 md:h-44 xl:h-28">Hi there, I’m <span className="name">Khanh </span>- <span ref={el}></span></div>
           <div className="font-medium text-xl md:text-4xl leading-6 md:leading-10 pb-8 md:pb-10">a health tech enthusiast driven by the idea of integrating technology and healthcare to unveil novel solutions to health challenges ٩(๑❛ᴗ❛๑)۶</div>
           <div className="flex justify-start gap-x-6">
-            <Link href="https://www.linkedin.com/in/baokhanhle/" target="_blank"><BsLinkedin className="icon" size="36px" alt="LinkedIn" /></Link>
-            <Link href="https://github.com/bxokhxnhle" target="_blank"><BsGithub className="icon" size="36px" alt="GitHub" /></Link>
-            <Link href="mailto:khanh.le@uwaterloo.ca"><BsFillEnvelopeAtFill className="icon" size="36px" alt="Email" /></Link>
-            <Link href="https://drive.google.com/file/d/1Um-_rE17upYQryH7cFDOwAjfnY5Er44-/view?usp=sharing" target="_blank"><BsFileEarmarkArrowDownFill className="icon" size="36px" alt="Resume" /></Link>
+            <Link href="https://www.linkedin.com/in/baokhanhle/" target="_blank"><BsLinkedin className="icon" size="36px" /></Link>
+            <Link href="https://github.com/bxokhxnhle" target="_blank"><BsGithub className="icon" size="36px" /></Link>
+            <Link href="mailto:khanh.le@uwaterloo.ca"><BsFillEnvelopeAtFill className="icon" size="36px" /></Link>
+            <Link href="https://drive.google.com/file/d/1Um-_rE17upYQryH7cFDOwAjfnY5Er44-/view?usp=sharing" target="_blank"><BsFileEarmarkArrowDownFill className="icon" size="36px" /></Link>
           </div>
         <div className="text-lg pt-14 md:pt-20">Check out my adventure ↓</div>
         </section>
